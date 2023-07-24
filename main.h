709d3710789b001c37f3f97b5e9ce9ade3f711ad
johntymuriithi <muriithijohn634@gmail.com>
2023-07-24 01:55:08 -0700

    made a change in the files

--- main.h
+++ main.h
@@ -9,19 +9,14 @@
 
 struct convert
 {
-        char *sym;
-        int (*f)(va_list);
+	char *sym;
+	int (*f)(va_list);
 };
 typedef struct convert conver_t;
 
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-void _vprintf(const char *format, va_list args);
-int print_unsgined_number(unsigned int n);
-int print_number(va_list);
-int p_integer(va_list);
-int parser(const char *format, conver_t funct_list[], va_list args);
 int p_char(va_list);
 int p_string(va_list args);
 int p_percent(va_list);
--- printf.c
+++ printf.c
@@ -1,29 +1,76 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
 #include "main.h"
-#include <stdint.h>
+
+int print_char(int c)
+{
+	return (write(1, &c, 1));
+}
+
+int print_string(const char *str)
+{
+	int len = 0;
+
+	if (str == NULL)
+	{
+		return (write(1, "null", 6));
+	}
+	while (str[len])
+		len++;
+	return (write(1, str, len));
+}
 
 int _printf(const char *format, ...)
 {
-	int printed_chars;
-
-	conver_t funct_list[] = {
-		{"c", p_char},
-		{"s", p_string},
-		{"%", p_percent},
-		{"d", p_integer},
-		{"i", p_integer},
-		{NULL, NULL}
-	};
-
 	va_list args;
-
+	int count = 0;
+	char ch, *str, percent;
 
 	if (format == NULL)
+	{
 		return (-1);
-
+	}
+	if (!format || (format[0] == '%' && !format[1]))
+                return (-1);
+        if (format[0] == '%' && format[1] == ' ' && !format[2])
+                return (-1);
 	va_start(args, format);
+	for (; *format; format++)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0') {
+				count += write(1, format - 1, 1);
+				break;
+			}
+			if (*format == 'c')
+			{
+				ch = va_arg(args, int);
+				count += print_char(ch);
 
-	printed_chars = parser(format, funct_list, args);
+			}
+			else if (*format == 's')
+			{
+				str = va_arg(args, char*);
+				count += print_string(str);
+			}
+			else if (*format == '%')
+			{
+				percent = '%';
+				count += write(1, &percent, 1);
+			}
+			else
+			{
+				count += write(1, format - 1, 1);
+			}
+		}
+		else
+		{
+			count += write(1, format, 1);
+		}
+	}
 	va_end(args);
-
-	return (printed_chars);
+	return (count);
 }
