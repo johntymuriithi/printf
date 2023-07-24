28be3527d74dc10862f912c8beecb865a3e573d9
johntymuriithi <muriithijohn634@gmail.com>
2023-07-24 03:55:57 -0700

    changed a file

--- printf.c
+++ printf.c
@@ -31,7 +31,10 @@ int _printf(const char *format, ...)
 	{
 		return (-1);
 	}
-
+	if (!format || (format[0] == '%' && !format[1]))
+                return (-1);
+        if (format[0] == '%' && format[1] == ' ' && !format[2])
+                return (-1);
 	va_start(args, format);
 	for (; *format; format++)
 	{
