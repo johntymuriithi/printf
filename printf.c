d1b489208a5c6c045008e6e9ef4fad22abc60dd4
Nimoh53 <faithnimoh53@gmail.com>
2023-07-24 11:43:30 -0700

    printf.c

--- printf.c
+++ printf.c
@@ -1,14 +1,14 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include "main.h"
+#include <unistd.h>       
+#include <stdio.h>     
+#include <stdarg.h>      
+#include "main.h"         
 
 /**
- * print_char - This function prints character
- * @c: The character itself
- *
- * Return: returns the character passed
- */
+ * print_char - This function prints character  
+ * @c: The character itself   
+ *   
+ * Return: returns the character passed   
+ */     
 int print_char(int c)
 {
 	return (write(1, &c, 1));
