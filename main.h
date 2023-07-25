d619acab62f81f5e2483ea80cdf0123c19575b04
johntymuriithi <muriithijohn634@gmail.com>
2023-07-25 02:23:02 -0700

    refactored my files

--- main.h
+++ main.h
@@ -21,8 +21,6 @@ int p_char(va_list);
 int p_string(va_list args);
 int p_percent(va_list);
 int helper(int *count, const char *format, va_list args);
-int print_integer(int num);
 int print_int(int num, char sign, char space);
-int _printInt(const char *format, ...);
 
 #endif
